d0d7bbfd61a97bec4ab816d87ee3f580812a0ae1
sairamcse001 <sairam.pasupuleti@servicenow.com>
2025-08-06 14:38:36 +0530

    Terraform plan generated from ServiceNow

---
+++ terraform/generated/main.tf
@@ -0,0 +1,237 @@
+
+
+# -------- provider.tf --------
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# -------- variables.tf --------
+
+variable "app_name" {
+  type        = string
+  description = "Name of the application"
+  default     = "devops-genie"
+}
+
+# -------- main.tf --------
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.app_name}-vpc"
+  }
+}
+
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.app_name}-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "${var.app_name}-public-subnet-2"
+  }
+}
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.app_name}-igw"
+  }
+}
+
+
+resource "aws_eip" "nat_eip" {
+ vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+ allocation_id = aws_eip.nat_eip.id
+ subnet_id     = aws_subnet.public_1.id
+
+ tags = {
+   Name = "${var.app_name}-nat"
+ }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+ gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.app_name}-public-route-table"
+  }
+}
+
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.app_name}-main-bucket"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+ server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+ sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    id      = "auto_delete_after_30_days"
+    enabled = true
+
+    expiration {
+      days = 30
+    }
+  }
+
+  logging {
+ target_bucket = aws_s3_bucket.logs.id
+    target_prefix = "log/"
+  }
+}
+
+
+resource "aws_s3_bucket" "logs" {
+ bucket = "${var.app_name}-logs-bucket"
+  acl    = "private"
+
+  versioning {
+ enabled = true
+ }
+
+ server_side_encryption_configuration {
+    rule {
+ apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+ lifecycle_rule {
+    id      = "auto_delete_after_30_days"
+    enabled = true
+
+    expiration {
+      days = 30
+    }
+  }
+}
+
+
+
+resource "aws_lambda_function" "main" {
+ filename         = "lambda_function.zip" # Placeholder, replace with your actual zip file
+  function_name = "${var.app_name}-lambda"
+  role          = "arn:aws:iam::123456789012:role/lambda_basic_execution" # Replace with a proper IAM role ARN. Beyond the scope of this example.
+ handler        = "index.handler"
+ runtime        = "nodejs16.x"
+
+
+
+ dead_letter_config {
+ target_arn = aws_sqs_queue.dlq.arn
+ }
+}
+
+resource "aws_sqs_queue" "dlq" {
+  name = "${var.app_name}-dlq"
+}
+
+resource "aws_cloudwatch_log_group" "lambda" {
+ name = "/aws/lambda/${aws_lambda_function.main.function_name}"
+ retention_in_days = 30
+}
+
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.main.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.main.arn
+ events              = ["s3:ObjectCreated:*"]
+  }
+  depends_on = [aws_lambda_permission.allow_bucket_to_trigger_lambda]
+}
+
+
+
+resource "aws_lambda_permission" "allow_bucket_to_trigger_lambda" {
+ statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn   = aws_s3_bucket.main.arn
+}
+
+# -------- outputs.tf --------
+
+output "s3_bucket_main_arn" {
+  value = aws_s3_bucket.main.arn
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.main.arn
+}
+
+output "sqs_dlq_arn" {
+  value = aws_sqs_queue.dlq.arn
+}
+
+output "s3_bucket_logs_arn" {
+ value = aws_s3_bucket.logs.arn
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+# -------- terraform.tfvars --------
+
+app_name = "devops-genie"
