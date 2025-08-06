2ed4063c89e8de0b3cc970f852dcbb143ba2be16
sairamcse001 <sairam.pasupuleti@servicenow.com>
2025-08-06 13:53:52 +0530

    Terraform plan generated from ServiceNow

---
+++ terraform/generated/main.tf
@@ -0,0 +1,179 @@
+
+
+# -------- provider.tf --------
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# -------- variables.tf --------
+
+variable "project_name" {
+  type        = string
+  default     = "ride-sharing-app"
+  description = "Project name used for resource naming"
+}
+
+# -------- main.tf --------
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.102.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+resource "aws_eip" "nat_eip" {
+ vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+ allocation_id = aws_eip.nat_eip.id
+ subnet_id     = aws_subnet.public_1.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+
+resource "aws_s3_bucket" "homepage_bucket" {
+  bucket = "${var.project_name}-homepage"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_cloudfront_distribution" "homepage_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.homepage_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.homepage_bucket.bucket
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.homepage_bucket.bucket
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
+# -------- outputs.tf --------
+
+output "homepage_bucket_arn" {
+  value = aws_s3_bucket.homepage_bucket.arn
+}
+
+output "homepage_cdn_url" {
+  value = aws_cloudfront_distribution.homepage_cdn.domain_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+output "private_subnet_ids" {
+ value = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+}
+
+# -------- terraform.tfvars --------
+
+project_name = "ride-sharing-app"
