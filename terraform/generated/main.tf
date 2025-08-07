54fc1bb6bcafafdf952731f9bc602e07b5d234a9
sairamcse001 <sairam.pasupuleti@servicenow.com>
2025-08-07 09:09:32 +0530

    Terraform plan generated from ServiceNow

---
+++ terraform/generated/main.tf
@@ -0,0 +1,211 @@
+
+
+# -------- provider.tf --------
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+ region = "us-east-1"
+}
+
+# -------- variables.tf --------
+
+variable "app_name" {
+  type        = string
+  description = "Name of the application"
+  default     = "devops-genie"
+}
+
+# -------- main.tf --------
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+}
+
+resource "aws_nat_gateway" "nat" {
+ allocation_id = aws_eip.nat.id
+ subnet_id     = aws_subnet.public_1.id
+}
+
+resource "aws_eip" "nat" {
+ vpc = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+
+resource "aws_route_table_association" "public_1" {
+ subnet_id      = aws_subnet.public_1.id
+ route_table_id = aws_route_table.public.id
+}
+
+data "aws_availability_zones" "available" {}
+
+
+
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.app_name}-s3-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+ server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+ sse_algorithm = "AES256"
+      }
+    }
+ }
+
+ lifecycle_rule {
+    id      = "auto-delete-objects"
+    enabled = true
+    expiration {
+      days = 30
+    }
+  }
+
+
+ logging {
+    target_bucket = aws_s3_bucket.logs.id
+    target_prefix = "log/"
+ }
+}
+
+
+
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "${var.app_name}-s3-logs"
+
+
+  versioning {
+    enabled = true
+  }
+
+ server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+ sse_algorithm = "AES256"
+      }
+    }
+ }
+
+ lifecycle_rule {
+    id      = "auto-delete-objects"
+ enabled = true
+
+    expiration {
+ days = 30
+    }
+ }
+}
+
+
+
+
+resource "aws_sqs_queue" "dlq" {
+  name = "${var.app_name}-dlq"
+}
+
+
+
+resource "aws_lambda_function" "main" {
+ filename         = "lambda_function.zip"
+ function_name    = "${var.app_name}-lambda"
+  handler          = "index.handler"
+  runtime = "nodejs16.x"
+
+ s3_bucket = "your-bucket-name" # Replace with your bucket name
+  s3_key    = "lambda_function.zip" # Replace with your zip file name
+
+  source_code_hash = filebase64sha256("lambda_function.zip") # Generate SHA256 hash for local file
+
+
+  environment {
+ variables = {
+      KEY = "VALUE"
+    }
+  }
+ dead_letter_config {
+    target_arn = aws_sqs_queue.dlq.arn
+  }
+}
+
+
+
+resource "aws_cloudwatch_log_group" "lambda" {
+  name = "/aws/lambda/${aws_lambda_function.main.function_name}"
+  retention_in_days = 30
+}
+
+
+
+
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  bucket = aws_s3_bucket.main.id
+
+ lambda_function {
+    lambda_function_arn = aws_lambda_function.main.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+
+ depends_on = [aws_lambda_permission.allow_bucket]
+}
+
+
+
+
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn   = aws_s3_bucket.main.arn
+}
+
+# -------- outputs.tf --------
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.main.arn
+}
+
+output "lambda_function_arn" {
+ value = aws_lambda_function.main.arn
+}
+
+output "dlq_url" {
+ value = aws_sqs_queue.dlq.url
+}
+
+# -------- terraform.tfvars --------
+
+app_name = "devops-genie"
